a04dc18bc55d90b1e64919fe0e37d2f81bc81fe5
Solo <solomon258@gmail.com>
2022-09-19 14:34:06 +0530

    Simulator-Changes

--- pages/⚒ Project - FIFA 22 World Cup Predictor.py
+++ pages/⚒ Project - FIFA 22 World Cup Predictor.py
@@ -1,3 +1,5 @@
+import random
+from operator import add, sub
 import pandas as pd
 import numpy as np
 import joblib
@@ -45,8 +47,6 @@ def sidebar_bg(side_bg):
 side_bg = os.path.join(ROOT_DIR,'data',"SideBar.png")
 sidebar_bg(side_bg)
 
-
-
 st.header("FIFA 22 WORLD CUP PREDICTIONS ⚽")
 
 with st.sidebar.expander("! Information",expanded=True):
@@ -55,6 +55,13 @@ with st.sidebar.expander("! Information",expanded=True):
 with st.sidebar.expander("Project Links:",expanded=True):
     st.write("[Github](https://github.com/SolomonJesurathinam/FIFA-World-Cup-22-Predictions)")
 
+#Add randomeness factors to match
+def randomness(value):
+    ops = (add, sub)
+    op = random.choice(ops)
+    ran = random.randint(1,5)
+    ans = op(value,ran)
+    return ans
 
 
 list_2022 = np.array(['Qatar', 'Germany', 'Denmark', 'Brazil', 'France', 'Belgium', 'Croatia', 'Spain', 'Serbia', 'England', 'Switzerland', 'Netherlands', 'Argentina', 'IR Iran', 'Korea Republic', 'Japan', 'Saudi Arabia', 'Ecuador', 'Uruguay', 'Canada', 'Ghana', 'Senegal', 'Portugal', 'Poland', 'Tunisia', 'Morocco', 'Cameroon', 'USA', 'Mexico', 'Wales', 'Australia', 'Costa Rica'])
@@ -70,22 +77,22 @@ Team2_FIFA_RANK = st.text_input(label="Team2_FIFA_RANK",value=Team2Rank,disabled
 Team1GK = rankings[rankings["Team"] == Team1]['GK'].to_list()[0]
 Team1Def = rankings[rankings["Team"] == Team1]['DEF'].to_list()[0]
 Team1Att = rankings[rankings["Team"] == Team1]['ATT'].to_list()[0]
-Team1Off = rankings[rankings["Team"] == Team1]['MID'].to_list()[0]
+Team1Mid = rankings[rankings["Team"] == Team1]['MID'].to_list()[0]
 
-Team1_Goalkeeper_Score = st.text_input(label="Team1 Goalkeeper Score",value=Team1GK,disabled=True)
-Team1_Defense = st.text_input(label="Team1 Defense Score",value=Team1Def,disabled=True)
-Team1_Offense = st.text_input(label="Team1 Offense Score",value=Team1Att,disabled=True)
-Team1_Midfield = st.text_input(label="Team1 Midfield Score",value=Team1Off,disabled=True)
+Team1_Goalkeeper_Score = st.text_input(label="Team1 Goalkeeper Score",value=randomness(Team1GK),disabled=True)
+Team1_Defense = st.text_input(label="Team1 Defense Score",value=randomness(Team1Def),disabled=True)
+Team1_Offense = st.text_input(label="Team1 Offense Score",value=randomness(Team1Att),disabled=True)
+Team1_Midfield = st.text_input(label="Team1 Midfield Score",value=randomness(Team1Mid),disabled=True)
 
 Team2GK = rankings[rankings["Team"] == Team2]['GK'].to_list()[0]
 Team2Def = rankings[rankings["Team"] == Team2]['DEF'].to_list()[0]
 Team2Att = rankings[rankings["Team"] == Team2]['ATT'].to_list()[0]
-Team2Off = rankings[rankings["Team"] == Team2]['MID'].to_list()[0]
+Team2Mid = rankings[rankings["Team"] == Team2]['MID'].to_list()[0]
 
-Team2_Goalkeeper_Score = st.text_input(label="Team2 Goalkeeper Score",value=Team2GK,disabled=True)
-Team2_Defense = st.text_input(label="Team2 Defense Score",value=Team2Def,disabled=True)
-Team2_Offense = st.text_input(label="Team2 Offense Score",value=Team2Att,disabled=True)
-Team2_Midfield = st.text_input(label="Team2 Midfield Score",value=Team2Off,disabled=True)
+Team2_Goalkeeper_Score = st.text_input(label="Team2 Goalkeeper Score",value=randomness(Team2GK),disabled=True)
+Team2_Defense = st.text_input(label="Team2 Defense Score",value=randomness(Team2Def),disabled=True)
+Team2_Offense = st.text_input(label="Team2 Offense Score",value=randomness(Team2Att),disabled=True)
+Team2_Midfield = st.text_input(label="Team2 Midfield Score",value=randomness(Team2Mid),disabled=True)
 
 league = os.path.join(ROOT_DIR,'data',"League_Predictions.pkl")
 knockout = os.path.join(ROOT_DIR,'data',"KnockOut_Predictions.pkl")
@@ -128,3 +135,40 @@ if st.button("KnockOut Match Prediction"):
         percentage = probability[0][0]
         percentage = round(percentage * 100)
         st.success(Team2 + " has {}% chance to win the match".format(percentage))
+
+count_0 = 0
+count_1 = 0
+count_2 = 0
+
+if st.button("League Simulation - 1000 times"):
+    for i in range(1000):
+        list_value1 = [[Team1, Team2, Team1Rank, Team2Rank, randomness(Team1GK), randomness(Team2GK), randomness(Team1Def), randomness(Team1Att), randomness(Team1Mid), randomness(Team2Def), randomness(Team2Att), randomness(Team2Mid)]]
+        df1 = pd.DataFrame(data=list_value1, columns=col_names)
+        result = league_model.predict(df1)[0]
+        if result == 0:
+            count_0 = count_0 + 1
+        if result == 1:
+            count_1 = count_1 + 1
+        if result == 2:
+            count_2 = count_2 + 1
+    if((count_1> count_2) & (count_1 > count_0)):
+        st.success(Team1+" has {}% of winning chance in 1000 simulations".format(round((max(count_0,count_1,count_2)/1000)*100)))
+    elif((count_2 > count_1) & (count_2 > count_0)):
+        st.success("{}% chance for DRAW in 1000 simulations".format(round((max(count_0,count_1,count_2)/1000)*100)))
+    else:
+        st.success(Team2+" has {}% of winning chance in 1000 simulations".format(round((max(count_0,count_1,count_2)/1000)*100)))
+
+#Knockout Simulation
+if st.button("Knockout Simulation - 1000 times"):
+    for i in range(1000):
+        list_value1 = [[Team1, Team2, Team1Rank, Team2Rank, randomness(Team1GK), randomness(Team2GK), randomness(Team1Def), randomness(Team1Att), randomness(Team1Mid), randomness(Team2Def), randomness(Team2Att), randomness(Team2Mid)]]
+        df1 = pd.DataFrame(data=list_value1, columns=col_names)
+        result = knockout_model.predict(df1)[0]
+        if result == 0:
+            count_0 = count_0 + 1
+        if result == 1:
+            count_1 = count_1 + 1
+    if(count_1 > count_0):
+        st.success(Team1+" has {}% of winning chance in 1000 simulations".format(round((max(count_0,count_1)/1000)*100)))
+    else:
+        st.success(Team2+" has {}% of winning chance in 1000 simulations".format(round((max(count_0,count_1)/1000)*100)))
---
+++ pages/⚒ Project - FIFA 22 World Cup Simulator.py
@@ -0,0 +1,210 @@
+import pandas as pd
+import joblib
+import random
+from operator import add, sub
+import os
+import streamlit as st
+import base64
+
+st.set_page_config(initial_sidebar_state="auto",page_title="FIFA WORLD CUP 22 SIMULATOR",page_icon="⚽")
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+#Background Image
+def set_bg_hack(main_bg):
+    main_bg_ext = "png"
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+main_bg = os.path.join(ROOT_DIR,'data',"Upload.png")
+set_bg_hack(main_bg)
+
+def sidebar_bg(side_bg):
+   side_bg_ext = 'png'
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+side_bg = os.path.join(ROOT_DIR,'data',"SideBar.png")
+sidebar_bg(side_bg)
+
+st.header("FIFA 22 WORLD CUP SIMULATOR ⚽")
+
+
+sim_count = st.slider("Simulation Count",min_value=10,max_value=100,help="Number of times each match is simultated to get the result")
+random_value = st.slider("Randomness Value",min_value=2,max_value=10,help="Affects team performance value (positive & negative)")
+
+if st.button("Simulate FIFA WORLD CUP 2022"):
+    ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+    league = os.path.join(ROOT_DIR,'data',"League_Predictions.pkl")
+    knockout = os.path.join(ROOT_DIR,'data',"KnockOut_Predictions.pkl")
+    colNames = os.path.join(ROOT_DIR,'data',"col_names.pkl")
+    league_model = joblib.load(league)
+    knockout_model = joblib.load(knockout)
+    col_names = joblib.load(colNames)
+
+    excel_path = os.path.join(ROOT_DIR,'data',"FifaRankings.csv")
+    rankings = pd.read_csv(excel_path)
+
+    def randomness(value):
+        ops = (add, sub)
+        op = random.choice(ops)
+        ran = random.randint(1,random_value)
+        ans = op(value,ran)
+        return ans
+
+    def table(Title,list_value):
+        df1 = pd.DataFrame(data=list_value, columns=[Title])
+        styler = df1.style.hide(axis='index').applymap(lambda x: "background-color: hsla(89, 43%, 51%, 0.3)")
+        st.write(styler.to_html(), unsafe_allow_html=True)
+        st.write("")
+
+    def TeamList(Team1, Team2):
+        Team1_FIFA_RANK = rankings[rankings["Team"] == Team1]['Rank'].to_list()[0]
+        Team2_FIFA_RANK = rankings[rankings["Team"] == Team2]['Rank'].to_list()[0]
+        Team1_Goalkeeper_Score = randomness(rankings[rankings["Team"] == Team1]['GK'].to_list()[0])
+        Team1_Defense = randomness(rankings[rankings["Team"] == Team1]['DEF'].to_list()[0])
+        Team1_Offense = randomness(rankings[rankings["Team"] == Team1]['ATT'].to_list()[0])
+        Team1_Midfield = randomness(rankings[rankings["Team"] == Team1]['MID'].to_list()[0])
+        Team2_Goalkeeper_Score = randomness(rankings[rankings["Team"] == Team2]['GK'].to_list()[0])
+        Team2_Defense = randomness(rankings[rankings["Team"] == Team2]['DEF'].to_list()[0])
+        Team2_Offense = randomness(rankings[rankings["Team"] == Team2]['ATT'].to_list()[0])
+        Team2_Midfield = randomness(rankings[rankings["Team"] == Team2]['MID'].to_list()[0])
+        list_value = [[Team1, Team2, Team1_FIFA_RANK, Team2_FIFA_RANK, Team1_Goalkeeper_Score, Team2_Goalkeeper_Score, Team1_Defense, Team1_Offense, Team1_Midfield, Team2_Defense, Team2_Offense, Team2_Midfield]]
+        df = pd.DataFrame(data=list_value,columns=col_names)
+        return df
+
+    def league_model_result(Team1, Team2):
+        count_0=0
+        count_1=0
+        count_2=0
+        for i in range(sim_count):
+            result = league_model.predict(TeamList(Team1,Team2))
+            if result == 0:
+                count_0 = count_0 + 1
+            if result == 1:
+                count_1 = count_1 + 1
+            if result == 2:
+                count_2 = count_2 + 1
+        if((count_1> count_2) & (count_1 > count_0)):
+            return Team1
+        elif((count_2 > count_1) & (count_2 > count_0)):
+            return "Draw"
+        else:
+            return Team2
+
+    def League_round(Team1, Team2, Team3, Team4):
+        match1 = league_model_result(Team1, Team2)
+        match2 = league_model_result(Team1, Team3)
+        match3 = league_model_result(Team1, Team4)
+        match4 = league_model_result(Team2, Team3)
+        match5 = league_model_result(Team2, Team4)
+        match6 = league_model_result(Team3, Team4)
+        Points = [match1,match2,match3,match4,match5,match6]
+        Team1_points = Points.count(Team1) * 3
+        Team2_points = Points.count(Team2) * 3
+        Team3_points = Points.count(Team3) * 3
+        Team4_points = Points.count(Team4) * 3
+        if match1 == "Draw":
+            Team1_points,Team2_points = Team1_points+1,Team2_points+1
+        if match2 == "Draw":
+            Team1_points,Team3_points = Team1_points+1,Team3_points+1
+        if match3 == "Draw":
+            Team1_points,Team4_points = Team1_points+1,Team4_points+1
+        if match4 == "Draw":
+            Team2_points,Team3_points = Team2_points+1,Team3_points+1
+        if match5 == "Draw":
+            Team2_points,Team4_points = Team2_points+1,Team4_points+1
+        if match6 == "Draw":
+            Team3_points,Team4_points = Team3_points+1,Team4_points+1
+        dict = {Team1:Team1_points,Team2:Team2_points,Team3:Team3_points,Team4:Team4_points}
+        grp_winners = pd.DataFrame(list(dict.items()),columns=['Team','Points']).sort_values('Points',ascending=False)[0:2]['Team']
+        return grp_winners
+
+    Grp1A, Grp2A = tuple(League_round("Qatar", "Ecuador","Senegal","Netherlands"))
+    table("Group A Winners",[Grp1A, Grp2A])
+    Grp1B, Grp2B = tuple(League_round("England","IR Iran", "USA", "Wales"))
+    table("Group B Winners", [Grp1B, Grp2B])
+    Grp1C, Grp2C = tuple(League_round("Argentina","Saudi Arabia", "Mexico","Poland"))
+    table("Group C Winners", [Grp1C, Grp2C])
+    Grp1D, Grp2D = tuple(League_round("France","Australia","Denmark","Tunisia"))
+    table("Group D Winners", [Grp1D, Grp2D])
+    Grp1E, Grp2E = tuple(League_round("Spain","Costa Rica","Germany","Japan"))
+    table("Group E Winners", [Grp1E, Grp2E])
+    Grp1F, Grp2F = tuple(League_round("Belgium","Canada","Morocco","Croatia"))
+    table("Group F Winners", [Grp1F, Grp2F])
+    Grp1G, Grp2G = tuple(League_round("Brazil","Serbia","Switzerland","Cameroon"))
+    table("Group G Winners", [Grp1G, Grp2G])
+    Grp1H, Grp2H = tuple(League_round("Portugal","Ghana","Uruguay","Korea Republic"))
+    table("Group H Winners", [Grp1H, Grp2H])
+    #table("Round of 16",[Grp1A,Grp2A,Grp1B,Grp2B,Grp1C,Grp2C,Grp1D,Grp2D,Grp1E,Grp2E,Grp1F,Grp2F,Grp1G,Grp2G,Grp1H,Grp2H])
+
+    def knockout_model_result(Team1, Team2):
+        count_0=0
+        count_1=0
+        for i in range(sim_count):
+            result = knockout_model.predict(TeamList(Team1,Team2))
+            if result == 0:
+                count_0 = count_0 + 1
+            if result == 1:
+                count_1 = count_1 + 1
+        if(count_1 > count_0):
+            return Team1
+        else:
+            return Team2
+
+    st.subheader("Round of 16")
+
+    W51 = knockout_model_result(Grp1B,Grp2A)
+    table((Grp1B + " VS " + Grp2A),[W51])
+    W52 = knockout_model_result(Grp1A,Grp2B)
+    table((Grp1A + " VS " + Grp2B), [W52])
+    W55 = knockout_model_result(Grp1D,Grp2C)
+    table((Grp1D + " VS " + Grp2C), [W55])
+    W56 = knockout_model_result(Grp1C,Grp2D)
+    table((Grp1C + " VS " + Grp2D), [W56])
+    W53 = knockout_model_result(Grp1F,Grp2E)
+    table((Grp1F + " VS " + Grp2E), [W53])
+    W54 = knockout_model_result(Grp1E,Grp2F)
+    table((Grp1E + " VS " + Grp2F), [W54])
+    W49 = knockout_model_result(Grp1H,Grp2G)
+    table((Grp1H + " VS " + Grp2G), [W49])
+    W50 = knockout_model_result(Grp1G,Grp2H)
+    table((Grp1G + " VS " + Grp2H), [W50])
+
+    st.subheader("Round of 8 - Quarter-Finals")
+
+    W59 = knockout_model_result(W51,W52)
+    table((W51 + " VS " + W52), [W59])
+    W60 = knockout_model_result(W55,W56)
+    table((W55 + " VS " + W56), [W60])
+    W57 = knockout_model_result(W49,W50)
+    table((W49 + " VS " + W50), [W57])
+    W58 = knockout_model_result(W53,W54)
+    table((W53 + " VS " + W54), [W58])
+
+    st.subheader("Semi-Finals")
+
+    final1 = knockout_model_result(W59,W60)
+    table((W59 + " VS " + W60), [final1])
+    final2 = knockout_model_result(W57,W58)
+    table((W57 + " VS " + W58), [final2])
+
+    st.header("Finals")
+    final = knockout_model_result(final1,final2)
+    table((final1 + " VS " + final2), [final])
+    st.success(final+" Will win the World Cup 2022")
+
