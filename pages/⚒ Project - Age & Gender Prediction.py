2a70963d927402fd90eb4407ab966b76ad0324a1
Solo <solomon258@gmail.com>
2022-09-28 00:29:09 +0530

    AgeGender-V1

--- pages/⚒ Project - Age & Gender Prediction.py
+++ pages/⚒ Project - Age & Gender Prediction.py
@@ -1,3 +1,4 @@
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 import cv2
 import os
 from tensorflow import keras
@@ -10,36 +11,34 @@ gender = os.path.join(ROOT_DIR,'data/models',"GenderPrediction1.h5")
 classifier = os.path.join(ROOT_DIR,'data',"haarcascade_frontalface_alt.xml")
 
 import streamlit as st
-st.title("Age Gender")
-FRAME_WINDOW = st.image([])
+st.title("Age Gender Prediction")
 
-if st.button("Detect Face"):
+gender_model = keras.models.load_model(gender)
 
-    gender_model = keras.models.load_model(gender)
-    def predict_image(predict_img):
-        x = cv2.resize(predict_img, (48, 48))
-        x = x.reshape(-1,48,48,1)
-        test_image = ImageDataGenerator(rescale=1./255.0).flow(x)
-        pred_value = np.argmax(gender_model.predict(test_image))
-        if pred_value == 0:
-            return "Male"
-        if pred_value == 1:
-            return "Female"
+def predict_image(predict_img):
+    x = cv2.resize(predict_img, (48, 48))
+    x = x.reshape(-1, 48, 48, 1)
+    test_image = ImageDataGenerator(rescale=1. / 255.0).flow(x)
+    pred_value = np.argmax(gender_model.predict(test_image))
+    if pred_value == 0:
+        return "Male"
+    if pred_value == 1:
+        return "Female"
 
-    age_model = keras.models.load_model(age)
-    def age_image(predict_img):
-        x = cv2.resize(predict_img, (48, 48))
-        x = x.reshape(-1,48,48,1)
-        op = ImageDataGenerator(rescale=1./255.0).flow(x)
-        predictions = age_model.predict(op)
-        return predictions
+age_model = keras.models.load_model(age)
+def age_image(predict_img):
+    x = cv2.resize(predict_img, (48, 48))
+    x = x.reshape(-1,48,48,1)
+    op = ImageDataGenerator(rescale=1./255.0).flow(x)
+    predictions = age_model.predict(op)
+    return predictions
 
-    cap = cv2.VideoCapture(0)
-    faces_list = []
-    color_list = []
-    while True:
-        ret, image = cap.read()
 
+
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        image = frame.to_ndarray(format="bgr24")
         face_cascade = cv2.CascadeClassifier(classifier)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 5)
@@ -47,7 +46,6 @@ if st.button("Detect Face"):
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
             gray_face_img = gray[y:y + h, x:x + w].copy()
-            #color_face_img = image[y:y + h, x:x + w].copy()
 
             # Gender Prediction
             gender = predict_image(gray_face_img)
@@ -55,19 +53,10 @@ if st.button("Detect Face"):
 
             # Age Prediction
             age = age_image(gray_face_img)
-            min_value = round(age[0][0]) - 3
-            max_value = round(age[0][0]) + 3
-            cv2.putText(image, str("{}-{}".format(min_value,max_value)), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            #min_value = round(age[0][0]) - 3
+            #max_value = round(age[0][0]) + 3
+            cv2.putText(image, str(round(age[0][0])), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        cv2.imshow('inp', image)
-        #cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        c = cv2.waitKey(1)
-        if c == 27:
-            break
+        return image
 
-    cap.release()
-    cv2.destroyAllWindows()
-
-if st.button("Stop"):
-    st.stop()
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
