ddd6731ec8dbb9b221148e75d8dbeed845333f4c
Solo <solomon258@gmail.com>
2022-10-02 18:12:38 +0530

    AgeGender-V2

--- pages/⚒ Project - Age & Gender Prediction.py
+++ pages/⚒ Project - Age & Gender Prediction.py
@@ -6,7 +6,8 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-age = os.path.join(ROOT_DIR,'data/models',"AgePrediction.h5")
+age = os.path.join(ROOT_DIR,'data/models',"age_net.caffemodel")
+age_proto = os.path.join(ROOT_DIR,'data/models',"deploy_age.prototxt")
 gender = os.path.join(ROOT_DIR,'data/models',"GenderPrediction1.h5")
 classifier = os.path.join(ROOT_DIR,'data',"haarcascade_frontalface_alt.xml")
 
@@ -14,7 +15,8 @@ import streamlit as st
 st.title("Age Gender Prediction")
 
 gender_model = keras.models.load_model(gender,compile=False)
-age_model = keras.models.load_model(age,compile=False)
+age_model = cv2.dnn.readNet(age_proto, age)
+AGE_RANGE = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)","(38-43)", "(48-53)", "(60-100)"]
 
 def predict_image(predict_img):
     x = cv2.resize(predict_img, (48, 48))
@@ -26,12 +28,17 @@ def predict_image(predict_img):
     if pred_value == 1:
         return "Female"
 
-def age_image(predict_img):
-    x = cv2.resize(predict_img, (48, 48))
-    x = x.reshape(-1,48,48,1)
-    op = ImageDataGenerator(rescale=1./255.0).flow(x)
-    predictions = age_model.predict(op)
-    return predictions
+def age_model_func(image):
+    #face = cv2.imread(image)
+    faceBlob = cv2.dnn.blobFromImage(image, 1.0, (227, 227),(78.4263377603, 87.7689143744, 114.895847746),swapRB=False)
+    age_model.setInput(faceBlob)
+    preds = age_model.forward()
+    i = preds[0].argmax()
+    age = AGE_RANGE[i]
+    ageConfidence = preds[0][i]
+    text = "{}: {:.2f}%".format(age, ageConfidence * 100)
+    return text
+
 
 def predict(image):
     face_cascade = cv2.CascadeClassifier(classifier)
@@ -41,16 +48,15 @@ def predict(image):
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
         gray_face_img = gray[y:y + h, x:x + w].copy()
+        color_face_img = image[y:y + h, x:x + w].copy()
 
         # Gender Prediction
         gender = predict_image(gray_face_img)
         cv2.putText(image, gender, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         # Age Prediction
-        age = age_image(gray_face_img)
-        # min_value = round(age[0][0]) - 3
-        # max_value = round(age[0][0]) + 3
-        cv2.putText(image, str(round(age[0][0])), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        age = age_model_func(color_face_img)
+        cv2.putText(image, age, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
     return image
 
 def radio_functiom(input_image):
