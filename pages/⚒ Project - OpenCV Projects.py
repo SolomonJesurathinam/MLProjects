4eb78d9746f45963a28c2e53abdd303bcede763b
Solo <solomon258@gmail.com>
2022-10-14 21:14:35 +0530

    OpenCV

---
+++ pages/⚒ Project - OpenCV Projects.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Open CV Projects")
+
+
+st.warning("Due to space constraints OpenCV projects have been moved to another app, please click on the below link")
+"[OpenCV Project Link](https://solomonjesurathinam-opencvprojects-homepage-1ujplw.streamlitapp.com/)"
